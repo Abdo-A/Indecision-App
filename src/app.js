08f8d4328807f36241c4b443ba6a6d9a07f7e64c
Abdelrahman Mohamadeen <33037931+Abdo-A@users.noreply.github.com>
2018-01-05 08:06:08 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const onFormSubmit=(e)=>{
     e.preventDefault();
     const option=e.target.elements.option.value;
     if (option) {
-        console.log(option);
         app.options.push(option);
         e.target.elements.option.value='';
         render();
@@ -20,6 +19,12 @@ const onRemoveAll = ()=>{
     render();
 };
 
+const onMakeDecision = ()=>{
+    const randomNum= Math.floor(Math.random()*app.options.length);
+    const option= app.options[randomNum];
+    alert(option);
+};
+
 const appRoot=document.getElementById("app");
 
 const render=()=> {
@@ -28,7 +33,7 @@ const template= (
         <h1>{app.title}</h1>
         {app.subtitle&&<p>{app.subtitle}</p>}
         <p>{app.options.length>0? 'Here are your options:':'No options, sorry'}</p>
-        <p>{app.options.length}</p>
+        <button disabled={!app.options.length} onClick={onMakeDecision}>What should I do?</button>
         <button onClick={onRemoveAll}>Remove all</button>
         <ol>
             {app.options.map((number, index)=><li key={index}>{number}</li>)}
